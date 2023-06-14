fc8dab5a7150968af744b9f0a3dcb7bc8899728e
Nick McCurdy <nick@nickmccurdy.com>
2023-06-14 12:36:04 -0400

    Group by category

--- app/page.tsx
+++ app/page.tsx
@@ -3,58 +3,45 @@ import data from './data.json'
 import { metadata } from './layout'
 import './page.css'
 
-export default async function Home() {
-  const colors = {
-    category: new Map([
-      ['Framework', 'blue'],
-      ['Meta Framework', 'green'],
-      ['Tool', 'gray'],
-    ]),
-    status: new Map([
-      ['Planned', 'gray'],
-      ['Canary', 'yellow'],
-      ['Stable', 'green'],
-    ]),
-  }
+export default function Home() {
+  const statusColors = new Map([
+    ['Planned', 'gray'],
+    ['Canary', 'yellow'],
+    ['Stable', 'green'],
+  ])
 
   return (
     <article>
       <header>
         <h1>{metadata.title}</h1>
       </header>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Category</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map(({ name, category, status, url }) => (
+      {['Framework', 'Meta Framework', 'Tool'].map((sortCategory) => (
+        <>
+          <h2>{sortCategory}s</h2>
+          <table>
+            <thead>
               <tr>
-                <td>{url ? <a href={url}>{name}</a> : name}</td>
-                <td>
-                  <span
-                    className={[
-                      'selected-value',
-                      `color-${colors.category.get(category)}`,
-                    ].join(' ')}
-                  >
-                    {category}
-                  </span>
-                </td>
-                <td>
-                  <span className={`color-${colors.status.get(status)}`}>
-                    {status}
-                  </span>
-                </td>
+                <th>Name</th>
+                <th>Status</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {data
+                .filter(({ category }) => category === sortCategory)
+                .map(({ name, status, url }) => (
+                  <tr>
+                    <td>{url ? <a href={url}>{name}</a> : name}</td>
+                    <td>
+                      <span className={`color-${statusColors.get(status)}`}>
+                        {status}
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
+      ))}
     </article>
   )
 }
