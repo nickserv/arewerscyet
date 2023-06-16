e63cbf0a45336d81f493cfd977007c8501325a9d
Nick McCurdy <nick@nickmccurdy.com>
2023-06-15 21:58:25 -0400

    Install Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-22970573-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-22970573-1');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
