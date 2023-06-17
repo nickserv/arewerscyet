1c0e8a6141474b6539af8601e11192d9a7842bdf
Nick McCurdy <nick@nickmccurdy.com>
2023-06-17 01:41:58 -0400

    Use `Map.groupBy` for refactoring and performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import 'core-js/proposals/array-grouping-v2'
 import { Fragment } from 'react'
 import { metadata } from './layout'
 import './page.css'
 import data from './page.json'
+
+declare global {
+  interface ObjectConstructor {
+    groupBy<T>(
+      items: Iterable<T>,
+      callbackfn: (value: T, index: number) => string
+    ): Record<string, T[]>
+  }
+
+  interface MapConstructor {
+    groupBy<T, U>(
+      items: Iterable<T>,
+      callbackfn: (value: T, index: number) => U
+    ): Map<U, T[]>
+  }
+}
 
 export default function Home() {
   const statusEmoji = new Map([
@@ -14,12 +31,12 @@ export default function Home() {
   return (
     <>
       <h1>{metadata.title}</h1>
-      {['Framework', 'Meta Framework', 'Tool'].map((sortCategory) => (
-        <Fragment key={sortCategory}>
-          <h2>{sortCategory}s</h2>
-          {data
-            .filter(({ category }) => category === sortCategory)
-            .map(({ name, status, url }) => (
+      {Array.from(
+        Map.groupBy(data, ({ category }) => category),
+        ([category, data]) => (
+          <Fragment key={category}>
+            <h2>{category}s</h2>
+            {data.map(({ name, status, url }) => (
               <Fragment key={name}>
                 <h3>
                   <a href={url}>{name}</a>
@@ -29,8 +46,9 @@ export default function Home() {
                 </p>
               </Fragment>
             ))}
-        </Fragment>
-      ))}
+          </Fragment>
+        )
+      )}
     </>
   )
 }
